0116a7b92f44d64425faf767e18407039aba268b
Stefan <stolniceanu.stefan2012@gmail.com>
2014-04-02 08:25:56 +0300

    asd

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,7 @@
 class StaticPagesController < ApplicationController
   def Home
   end
-
-  def GlobalRanking
-	@scorings = Score.order(scoring: :desc)
-	
-  end
-
-  def LocalRanking
-  end
-
-  def Pong
-  end
-
+  
   def Documentation
   end
 end
