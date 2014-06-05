4b3ad45bf32d784bc35a077db5141367760218a2
Stefan <stolniceanu.stefan2012@gmail.com>
2014-06-05 18:22:49 +0300

    Devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Wagahahahgagahah::Application.routes.draw do
   
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get "api/NewScoring"
