fa9ca64cb73cba37f737de2ab5721510d6711a69
Stefan <stolniceanu.stefan2012@gmail.com>
2014-06-12 10:24:20 +0300

    Raw version of header created

--- db/schema.rb
+++ db/schema.rb
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 20140611212710) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "token"
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
